9c84add7910df74f5ee4bcd6a1e9a6e4f28bc836
ConstantE23 <econstant9235@gmail.com>
2026-04-22 09:33:19 -0400

    Updated API for server

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -25,7 +25,7 @@ const COLORS = {
   skyBlue: '#BAE6FD',
 };
 
-const API_URL = 'http://4.239.243.37:8000';
+const API_URL = 'http://127.0.0.1:3306'; //Possible Fix for the API?
 
 interface TicketData {
   id: number;
