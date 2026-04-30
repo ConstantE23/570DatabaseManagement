8f744d54d2b9ed84ae89f8a5c804e56d029f7d34
Makaila Williams <makaila.jabree@gmail.com>
2026-04-30 14:28:10 -0400

    update

--- src/frontend/main.tsx
+++ src/frontend/main.tsx
@@ -1,10 +1,10 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
+import StudentApp from './StudentApp';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <StudentApp />
   </StrictMode>,
 );
